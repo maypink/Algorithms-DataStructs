29adab85ab36725650a4315c1e155ee628a98b89
maypink <57305249+maypink@users.noreply.github.com>
2020-04-05 21:23:35 +0500

    Create B.cpp

---
+++ Sem1/Laba8/B.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+ 
+using namespace std;
+ 
+string check(vector<vector<int>>&matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((i == j) && (matrix[i][j] == 1)) return "NO";
+            if (matrix[i][j] != matrix[j][i]) return "NO";
+        }
+    }
+    return "YES";
+}
+int main()
+{
+    int n;
+    vector <vector<int>> matrix;
+    ifstream cin("input.txt");
+    cin >> n;
+    matrix.assign(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    ofstream cout("output.txt");
+    cout<<check(matrix, n);
+}
